3c34ebf1bccfba97073af0527ddb0edb3cf78bc1
Micah Geisel <micah@botandrose.com>
2020-05-01 08:31:25 -0700

    WIP

--- lib/active_record/order_by_ids.rb
+++ lib/active_record/order_by_ids.rb
@@ -4,10 +4,16 @@ require "active_record"
 module ActiveRecord
   module OrderByIds
     def order_by_ids ids
-      fragments = []
-      fragments += ["#{primary_key} NOT IN (#{ids.map(&:to_s).join(",")})"] if ids.any?
-      fragments += ids.reverse.map { |id| "#{primary_key}=#{id}" }
-      reorder(Arel.sql(fragments.join(", ")))
+      order_by(primary_key => ids)
+    end
+
+    def order_by hash
+      hash.reduce(reorder(1)) do |scope, (key, ids)|
+        fragments = []
+        fragments += ["#{key} NOT IN (#{ids.map(&:to_s).join(",")})"] if ids.any?
+        fragments += ids.reverse.map { |id| "#{key}=#{id}" }
+        scope.order!(Arel.sql(fragments.join(", ")))
+      end
     end
   end
 end
